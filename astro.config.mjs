a084f68cd90a8439095a3fa2f9a66f4566e25f8f
serenevoid <ajuph9224@gmail.com>
2023-07-26 01:06:30 +0530

    chore(dependencies): added react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [sitemap()],
+  site: 'https://example.com',
+  integrations: [sitemap(), react()]
 });
