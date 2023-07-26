c33a27295c9b7e28706b709d6d766b60efd3bc6d
serenevoid <ajuph9224@gmail.com>
2023-07-26 06:28:07 +0530

    chore(consts): added site info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import react from "@astrojs/react";
-import image from "@astrojs/image";
-import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
+import image from '@astrojs/image';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://serenevoid.github.io/',
   integrations: [sitemap(), react(), image(), tailwind()]
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'The Serene Void';
+export const SITE_DESCRIPTION = 'A humble developer\'s knowledge vault';
+export const HOMEPAGE_URL = "https://serenevoid.github.io/"
