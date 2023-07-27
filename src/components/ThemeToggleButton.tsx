3dc1b73020c52251afd0eaa00a68a32534708ef6
serenevoid <ajuph9224@gmail.com>
2023-07-27 20:50:50 +0530

    chore(eslint): lint fixes

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { IoSunny, IoMoon } from 'react-icons/io5/index'
+import { IoSunny, IoMoon } from 'react-icons/io5';
 
 const themes = ['light', 'dark']
 
