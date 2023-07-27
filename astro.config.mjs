69171f9fd6efa0115d9e84a888703a36c33f315d
serenevoid <ajuph9224@gmail.com>
2023-07-27 10:13:15 +0530

    chore(dependencies): removed @astrojs/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
-import image from '@astrojs/image';
 import tailwind from '@astrojs/tailwind';
 import addClasses from 'rehype-add-classes';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://serenevoid.github.io/',
-  integrations: [sitemap(), react(), image(), tailwind()],
+  integrations: [sitemap(), react(), tailwind()],
   markdown: {
     extendDefaultPlugins: true,
     rehypePlugins: [
