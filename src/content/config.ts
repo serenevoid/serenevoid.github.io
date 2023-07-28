203ce41e0ab1e9bf48309a8078e023c37b13a614
serenevoid <ajuph9224@gmail.com>
2023-07-28 14:02:11 +0530

    feat(tags): added tags pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,10 +10,7 @@ const blog = defineCollection({
 			.string()
 			.or(z.date())
 			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
+    tags: z.array(z.string()),
 		heroImage: z.string().optional(),
 	}),
 });
