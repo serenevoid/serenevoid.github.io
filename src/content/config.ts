367d002135e56f4b6e124d9b24cb021f383eaeea
serenevoid <ajuph9224@gmail.com>
2023-10-25 20:45:25 +0530

    chore(*): tab formatting removed

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,18 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-    tags: z.array(z.string()),
-		heroImage: z.string().optional(),
-	}),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z
+    .string()
+    .or(z.date())
+    .transform((val) => new Date(val)),
+      tags: z.array(z.string()),
+    heroImage: z.string().optional(),
+  }),
 });
 
 export const collections = { blog };
